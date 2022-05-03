ae99f42cd345fe42bac38ceaebf915f9590542a3
Gabriel Cari <g.cari@hotmail.com>
2022-05-03 14:44:52 -0300

    Feat: POST/sign-up receiving and validating 'name', 'email', 'password', 'repeat_password'.

--- src/index.js
+++ src/index.js
@@ -27,11 +27,82 @@ mongoClient.connect()
 
 // Joi schemas
 const userSchema = joi.object({
-    name: joi.string().required()
+    name: joi.string().required(),
+    email: joi.string().email().required(),
+    password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+    repeat_password: joi.ref('password')
 });
 const messageSchema = joi.object({
     from: joi.string().required(),
     to: joi.string().required(),
     text: joi.string().required(),
     type: joi.string().required(),
+});
+
+app.post("/sign-up", async (req, res) => {
+    const {name, email, password, repeat_password} = req.body;
+    const user = req.body;
+    
+    const {error} = userSchema.validate(user, {abortEarly: false});
+    if(error){
+        console.log(error.details.map(detail => detail.message)); // TODO: erase me
+        return res.status(409).send(error.details.map(detail => detail.message));
+        // return res.status(409).send("Nome deve ser string não vazio!");
+    }else if(password !== repeat_password){
+        return res.status(406).send(`Passwords must be equal!`);
+    }
+    res.send(200)
+    
+    // try{
+    //     nameAlreadyExist = await db.collection("participants").findOne({name});
+    //     if(nameAlreadyExist){
+    //         return res.status(409).send("O nome escolhido já existe!");
+    //     }
+
+    //     await db.collection("messages").insertOne({
+    //         from: name,
+    //         to: 'Todos',
+    //         text: 'entra na sala...',
+    //         type: 'status',
+    //         time: dayjs().format('HH:mm:ss')
+    //     });
+        
+    //     await db.collection("participants").insertOne({name, lastStatus: Date.now()});
+    //     res.sendStatus(201);
+    // }catch(e){
+    //     console.log("Error on POST /participants", e);
+    //     res.sendStatus(500);
+    // }
+});
+
+app.post("/participants", async (req, res) => {
+    const {name} = req.body;
+    let nameAlreadyExist = [];
+    
+    const validation = userSchema.validate({name}, {abortEarly: false});
+    if(validation.error){
+        console.log(validation.error.details.map(detail => detail.message)); // TODO: erase me
+        return res.status(422).send("Nome deve ser string não vazio!");
+    }
+    
+    try{
+        nameAlreadyExist = await db.collection("participants").findOne({name});
+        if(nameAlreadyExist){
+            return res.status(409).send("O nome escolhido já existe!");
+        }
+
+        await db.collection("messages").insertOne({
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs().format('HH:mm:ss')
+        });
+        
+        await db.collection("participants").insertOne({name, lastStatus: Date.now()});
+        res.sendStatus(201);
+    }catch(e){
+        console.log("Error on POST /participants", e);
+        res.sendStatus(500);
+    }
 });
