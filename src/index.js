37fde4a4281fb97aebff1f609a00452e7f7eed5f
Gabriel Cari <g.cari@hotmail.com>
2022-05-09 15:52:25 -0300

    Fix: cors 3.

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ const app = express();
 app.use(cors());
 app.use(json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 // Routers
 app.use(authRouter);
 app.use(transactionRouter);
